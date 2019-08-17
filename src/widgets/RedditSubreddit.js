97e683a380b9f36f93a3f3ba37f80e512d46cdd9
Anton Gunnarsson <gson.anton@hotmail.com>
2019-08-17 12:27:48 +0200

    Basic reddit subreddit widget

--- src/widgets/RedditSubreddit.js
+++ src/widgets/RedditSubreddit.js
@@ -1,22 +1,65 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
-const StyledDemo = styled.div`
+const StyledContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
+  overflow-x: hidden;
   height: 100%;
   border: 1px dashed palevioletred;
 `
 
+const Title = styled.h1`
+  font-size: 16px;
+  padding-left: 8px;
+`
+
+const PostList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  padding: 8px;
+`
+
+const Post = styled.li`
+  font-size: 16px;
+  height: 24px;
+  line-height: 16px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
 export default function RedditSubreddit({ subredditName }) {
+  const [posts, setPosts] = useState([])
+
   useEffect(() => {
     fetch(`https://www.reddit.com/r/${subredditName}/new.json?sort=new`)
       .then(response => response.json())
-      .then(result => console.log(result))
+      .then(({ data }) => {
+        const ps = data.children.map(({ data }) => ({
+          id: data.id,
+          title: data.title,
+          author: data.author,
+          href: `https://reddit.com${data.permalink}`
+        }))
+        console.log(data.children)
+        setPosts(ps)
+      })
   }, [subredditName])
 
   return (
-    <StyledDemo>
-      <h1>{subredditName}</h1>
-    </StyledDemo>
+    <StyledContainer>
+      <Title>/r/{subredditName}</Title>
+      <PostList>
+        {posts.map(p => (
+          <Post key={p.id}>
+            <a href={p.href}>{p.title}</a>
+          </Post>
+        ))}
+      </PostList>
+    </StyledContainer>
   )
 }
